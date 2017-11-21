dcd68d9a089530398c0acf7eff14230d929a3787
Enrique Juan de Dios <enrique.juandedios@avantica.net>
2017-11-21 16:19:39 -0500

    Added HTML template

--- main.go
+++ main.go
@@ -2,15 +2,38 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net"
 	"net/http"
 	"strconv"
 	"strings"
+	"time"
 )
 
+// PageVariables represent the variables those are sent to the page
+type PageVariables struct {
+	Date string
+	Time string
+}
+
 func root(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Ruling the World")
-	fmt.Println("Reload Page. Request /")
+	now := time.Now()
+	HomePageVars := PageVariables{
+		Date: now.Format("02-01-2015"),
+		Time: now.Format("15:04:04"),
+	}
+
+	t, err := template.ParseFiles("home.html")
+
+	if err != nil {
+		log.Print("Template parsing error: ", err)
+	}
+	err = t.Execute(w, HomePageVars)
+
+	if err != nil {
+		log.Print("Template excuting error: ", err)
+	}
 }
 
 func main() {
